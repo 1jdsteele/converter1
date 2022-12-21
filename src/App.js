c1fb1913cc2e9776f776b9981c5ee752f3e5b42c
jdsteele <jdsteelepersonal@gmail.com>
2022-12-20 19:35:42 -0800

    react set up input created and showing as expected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="converter">
+       this should be before the input
+       <input type="text" className="input" />
+     </div>
     </div>
   );
 }
